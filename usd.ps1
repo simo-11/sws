7694b342c598426af5b91ae74e1a58d93f692407
Simo Nikula <simo.nikula@gmail.com>
2023-07-23 18:28:53 +0300

    Added check for python

--- usd.ps1
+++ usd.ps1
@@ -3,6 +3,10 @@ $pxr="$bp\lib\python\pxr"
 $env:PYTHONPATH="$bp\lib\python;$bp\lib;$pxr\Tf"
 $env:PATH="$env:PATH;$bp\lib"
 $pi="$bp\python\python"
+if(!(Test-Path -Path $pi)){
+    Write-Host "Python is not available at $pi"
+    return
+}
 $cmd,$cmdargs=$args
 $fp="$bp\bin"
 $fullCmd="$fp\$cmd"
