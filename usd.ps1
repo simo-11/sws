00051703c30708b9613b4f4c4de677730f724064
Simo Nikula <simo.nikula@gmail.com>
2023-07-23 18:20:01 +0300

    Initial usd wrapper

---
+++ usd.ps1
@@ -0,0 +1,15 @@
+$bp="C:\Users\simon\apps\usd"
+$pxr="$bp\lib\python\pxr"
+$env:PYTHONPATH="$bp\lib\python;$bp\lib;$pxr\Tf"
+$env:PATH="$env:PATH;$bp\lib"
+$pi="$bp\python\python"
+$cmd,$cmdargs=$args
+$fp="$bp\bin"
+$fullCmd="$fp\$cmd"
+if (Test-Path -Path $fullCmd -PathType Leaf) {
+    & $pi $fullCmd $cmdargs
+}else{
+    Write-Host "$cmd is not available at $fp"
+    Write-Host "PYTHONPATH=$env:PYTHONPATH"
+    & $pi
+}
